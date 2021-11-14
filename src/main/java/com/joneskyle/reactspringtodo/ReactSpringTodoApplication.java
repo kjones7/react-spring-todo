39b8779666cfb4c186344861df8e11867826a3af
Kyle Jones <mynameiskyle242@gmail.com>
2021-11-14 12:29:46 -0500

    Create spring project

---
+++ src/main/java/com/joneskyle/reactspringtodo/ReactSpringTodoApplication.java
@@ -0,0 +1,13 @@
+package com.joneskyle.reactspringtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactSpringTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joneskyle/reactspringtodo/ReactSpringTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joneskyle.reactspringtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

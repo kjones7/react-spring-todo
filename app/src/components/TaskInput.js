5987b7d3eabc6b00875f001d4aad650811fee97d
Kyle Jones <mynameiskyle242@gmail.com>
2021-11-27 11:42:44 -0500

    Move details related to adding tasks to `TaskInput.js`

--- app/src/components/TaskInput.js
+++ app/src/components/TaskInput.js
@@ -1,6 +1,14 @@
 function TaskInput(props) {
+  let addTask = function(e) {
+    let task = e.target.querySelector('.add-task-input').value;
+
+    e.preventDefault();
+
+    props.setTasks([...props.tasks, task]);
+  }
+
   return (
-      <form onSubmit={props.addTaskHandler}>
+      <form onSubmit={addTask}>
         <label>Task</label>
         <input className="add-task-input"/>
         <button>Add</button>
--- app/src/components/ToDoForm.js
+++ app/src/components/ToDoForm.js
@@ -4,21 +4,13 @@ import TaskInput from './TaskInput.js';
 function ToDoForm() {
   const [tasks, setTasks] = useState([]);
 
-  let addTask = function(e) {
-    let task = e.target.querySelector('.add-task-input').value;
-
-    e.preventDefault();
-
-    setTasks([...tasks, task]);
-  }
-
   const taskListItems = tasks.map((task, index) =>
       <li key={index}>{task}</li>
   );
 
   return (
       <div>
-        <TaskInput addTaskHandler={addTask} />
+        <TaskInput tasks={tasks} setTasks={setTasks} />
         <div>
           <ul>
             {taskListItems}

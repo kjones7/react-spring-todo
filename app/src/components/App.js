863efa613e93bd583b313d4cd13aeec10cae44b4
Kyle Jones <mynameiskyle242@gmail.com>
2021-11-27 11:10:31 -0500

    Move `ToDoForm` component to its own file

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,40 +1,9 @@
 import '../App.css';
-import { useState } from 'react';
+import ToDoForm from "./ToDoForm";
 
 function App() {
   return (
       <ToDoForm/>
-  );
-}
-
-function ToDoForm() {
-  const [tasks, setTasks] = useState([]);
-
-  let addTask = function(e) {
-    let task = e.target.querySelector('.add-task-input').value;
-
-    e.preventDefault();
-
-    setTasks([...tasks, task]);
-  }
-
-  const taskListItems = tasks.map((task, index) =>
-    <li key={index}>{task}</li>
-  );
-
-  return (
-      <div>
-        <form onSubmit={addTask}>
-          <label>Task</label>
-          <input className="add-task-input"/>
-          <button>Add</button>
-        </form>
-        <div>
-          <ul>
-            {taskListItems}
-          </ul>
-        </div>
-      </div>
   );
 }
 
---
+++ app/src/components/ToDoForm.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+function ToDoForm() {
+  const [tasks, setTasks] = useState([]);
+
+  let addTask = function(e) {
+    let task = e.target.querySelector('.add-task-input').value;
+
+    e.preventDefault();
+
+    setTasks([...tasks, task]);
+  }
+
+  const taskListItems = tasks.map((task, index) =>
+      <li key={index}>{task}</li>
+  );
+
+  return (
+      <div>
+        <form onSubmit={addTask}>
+          <label>Task</label>
+          <input className="add-task-input"/>
+          <button>Add</button>
+        </form>
+        <div>
+          <ul>
+            {taskListItems}
+          </ul>
+        </div>
+      </div>
+  );
+}
+
+export default ToDoForm;

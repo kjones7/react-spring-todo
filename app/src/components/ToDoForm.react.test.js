1ab0f817a474c150c2d5410e0be53cd7c247572c
Kyle Jones <mynameiskyle242@gmail.com>
2022-01-22 12:37:53 -0500

    Add `ToDoForm` test for multiple tasks and re-organize other tests

--- app/src/components/ToDoForm.react.test.js
+++ app/src/components/ToDoForm.react.test.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from "react-dom";
-import renderer from 'react-test-renderer';
 import { act } from "react-dom/test-utils";
 import ToDoForm from './ToDoForm';
-
-// Snapshot tests
-test('Initial snapshot', () => {
-  const component = renderer.create(
-      <ToDoForm />,
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
 
 // Other tests
 test('Fetch and display single task', async () => {
@@ -43,4 +33,43 @@ test('Fetch and display single task', async () => {
   unmountComponentAtNode(container);
   container.remove();
   container = null;
+  global.fetch.mockRestore();
+});
+
+test('Fetch and display multiple tasks', async () => {
+  // setup a DOM element as a render target
+  let container = document.createElement("div");
+  document.body.appendChild(container);
+
+  const fakeTasks = [
+      {
+        id: 1,
+        description: 'Task 1',
+      },{
+        id: 2,
+        description: 'Task 2',
+      }
+  ];
+
+  global.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(fakeTasks)
+    })
+  );
+
+  // Use the asynchronous version of act to apply resolved promises
+  await act(async () => {
+    render(<ToDoForm />, container);
+  });
+
+  // Assert
+  expect(container.querySelectorAll('li').length).toBe(2);
+  expect(container.querySelectorAll('li')[0].textContent).toBe(fakeTasks[0].description);
+  expect(container.querySelectorAll('li')[1].textContent).toBe(fakeTasks[1].description);
+
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+  global.fetch.mockRestore();
 });

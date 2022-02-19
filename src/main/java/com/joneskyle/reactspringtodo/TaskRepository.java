c20b6c6dc6c1668c402edbce8e7cc45c829e735b
Kyle Jones <mynameiskyle242@gmail.com>
2022-02-19 15:15:04 -0500

    Remove some old comments from java files

--- src/main/java/com/joneskyle/reactspringtodo/TaskRepository.java
+++ src/main/java/com/joneskyle/reactspringtodo/TaskRepository.java
@@ -4,9 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.joneskyle.reactspringtodo.Task;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called taskRepository
-// CRUD refers Create, Read, Update, Delete
-
 public interface TaskRepository extends CrudRepository<Task, Integer> {
 
 }
